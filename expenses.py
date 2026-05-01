3f38a50cbbb024aa0e7cc9b042d600878bdc9348
Malukha Arkadii <malukha.a.a@edu.mirea.ru>
2026-05-01 12:31:47 +0300

    добавлена функция регистрации расходов

--- expenses.py
+++ expenses.py
@@ -13,3 +13,25 @@ def add_category(args):
         data['categories'].append(args.category)
         save_data(data)
         print(f"Категория {args.category} успешно добавлена.")
+
+def add_expense(args):
+    data = load_data()
+    
+    try:
+        cost = float(args.cost)
+    except ValueError:
+        print("Ошибка: Стоимость должна быть числом.")
+        return
+
+    if args.category not in data['categories']:
+        print(f"Ошибка: Категории '{args.category}' не существует.")
+        return
+
+    new_expense = {
+        "cost": cost,
+        "category": args.category,
+        "name": args.name
+    }
+    data['expenses'].append(new_expense)
+    save_data(data)
+    print(f"Добавлен расход: {args.name} ({cost})")  
